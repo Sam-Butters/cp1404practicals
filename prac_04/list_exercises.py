1dffdd9359145a621ac654c3500e355b7bdabbcb
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-06-14 13:55:46 +1000

    Add woefully inadequate security checker program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,7 +1,8 @@
 """
-List operations
+List operations & Woefully inadequate security checker
 """
 
+# List exercises
 NUMBER_OF_REPETITIONS = 5
 
 numbers = []
@@ -13,3 +14,13 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average number is {sum(numbers) / NUMBER_OF_REPETITIONS}")
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input('Enter your username: ')
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
