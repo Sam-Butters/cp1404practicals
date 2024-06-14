2ade7dd539faac46ff2dcab94e5d98dab9c1c046
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-06-14 13:36:43 +1000

    Update program to read and process file data and display data in human-readable format

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,33 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Read and reformat a file"""
     data = load_data()
     print(data)
+    display_subject_details(data)
 
 
 def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read data from file and return it as a nested list"""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(",")
+        parts[2] = int(parts[2])
+        data.append(parts)
+
     input_file.close()
+    return data
+
+
+def display_subject_details(data):
+    """Display formatted subject details"""
+    for subject_detail in data:
+        subject_name = subject_detail[0]
+        lecturer = subject_detail[1]
+        number_of_students = subject_detail[2]
+        print(f"{subject_name} is taught by {lecturer} and has {number_of_students} students")
 
 
 main()
