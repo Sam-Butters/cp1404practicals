19798673caf8066a0080f5c41af60904b93fa43d
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-06-21 15:54:02 +1000

    Update with working word count dictionary

--- prac_05/word_occurances.py
+++ prac_05/word_occurances.py
@@ -3,5 +3,23 @@ CP1404 Practical 5 - Word Occurrences
 Sam Butters
 
 Estimate: 25 minutes
-Actual:
+Actual: 26:33
 """
+
+from operator import itemgetter
+
+word_to_count = {}
+
+word_string = input("Enter a string: ")
+words = word_string.split(" ")
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+
+max_word_length = max(len(word) for word in word_to_count)
+sorted_word_to_count = sorted(word_to_count.items(), key=itemgetter(1), reverse=True)
+
+for word, count in sorted_word_to_count:
+    print(f"{word:{max_word_length}} : {count}")
