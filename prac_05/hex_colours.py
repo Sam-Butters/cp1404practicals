fc2501c03f4678a731e3859b0bc06a1ebb4bb357
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-06-21 15:22:41 +1000

    Update dictionary name for clarity

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,14 +3,14 @@ CP1404 Practical 5 - Hex Colours
 Sam Butters
 """
 
-hex_colours = {"black": "#000000", "grey": "#808080", "white": "#ffffff", "red": "#ff0000", "orange": "#ffa500",
+colour_to_hexcode = {"black": "#000000", "grey": "#808080", "white": "#ffffff", "red": "#ff0000", "orange": "#ffa500",
                "yellow": "#ffff00",
                "green": "#008000", "blue": "0000ff", "indigo": "#4b0082", "violet": "#ee82ee"}
 
 colour_name = input("Enter colour name: ").lower()
 while colour_name != "":
     try:
-        print(f"Colour: {colour_name}  Hex code: {hex_colours[colour_name]}")
+        print(f"Colour: {colour_name}  Hex code: {colour_to_hexcode[colour_name]}")
     except KeyError:
         print(f"{colour_name} is not on the list")
     colour_name = input("Enter colour name: ").lower()
