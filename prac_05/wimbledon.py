b9ebe82a0102d9f32f3befde1248894b7e432fc6
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-06-23 13:38:31 +1000

    Refactor code to include functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,31 +2,52 @@
 CP1404 Practical 5 - Wimbledon
 
 """
-from operator import itemgetter
-
-champion_to_win_number = {}
-champion_countries = set()
-
-with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
-    lines = in_file.readlines()
-
-for line in lines[1:]:
-    data = line.split(",")
-    champion = data[2]
-    country = data[1]
-    champion_countries.add(country)
-    if champion in champion_to_win_number:
-        champion_to_win_number[champion] += 1
-    else:
-        champion_to_win_number[champion] = 1
+FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 
-print("Wimbledon Champions: ")
-for champion, win_number in champion_to_win_number.items():
-    print(f"{champion}: {win_number}")
+def main():
+    """Get data from a file, process and display results."""
+    lines = get_records(FILENAME)
+    champion_to_win_number, champion_countries = process_data(lines)
+    print_results(champion_to_win_number, champion_countries)
 
-# TODO: Get the champions list, grab their countries, print countries in alphabetical order with no repeats
-# Format should be in capitals and their 3 letter abbreviation
+
+def get_records(filename):
+    """Read file and return list of lines."""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        lines = in_file.readlines()
+    return lines
 
 
-print(champion_countries)
+def process_data(lines):
+    """Process data, returning a dictionary and a set."""
+    champion_to_win_number = {}
+    champion_countries = set()
+
+    for line in lines[1:]:  # Skip the first line (headers)
+        data = line.split(",")
+        champion = data[CHAMPION_INDEX].strip()
+        country = data[COUNTRY_INDEX].strip()
+        champion_countries.add(country)
+        if champion in champion_to_win_number:
+            champion_to_win_number[champion] += 1
+        else:
+            champion_to_win_number[champion] = 1
+
+    return champion_to_win_number, champion_countries
+
+
+def print_results(champion_to_win_number, champion_countries):
+    """Print the champions and their win numbers, and their respective countries."""
+    print("Wimbledon Champions:")
+    for champion, win_number in champion_to_win_number.items():
+        print(f"{champion}: {win_number}")
+
+    print(f"\nThese {len(champion_countries)} countries have won Wimbledon:")
+    countries = ", ".join(sorted(champion_countries))
+    print(countries)
+
+
+main()
