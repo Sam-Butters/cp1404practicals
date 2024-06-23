b2cd36973f372d88a932bfa78cf5b73da086fd44
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-06-23 13:09:28 +1000

    Add a set containing the countries of winners

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,23 +5,28 @@ CP1404 Practical 5 - Wimbledon
 from operator import itemgetter
 
 champion_to_win_number = {}
+champion_countries = set()
 
 with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
-    for line in in_file:
-        data = line.split(",")
-        champion = data[2]
-        # TODO: rewrite to skip first line containing headings, this will stop the output displaying
-        # "Champion at the top"
-        # Possibly use readlines instead of for line in infile
+    lines = in_file.readlines()
 
-        if champion in champion_to_win_number:
-            champion_to_win_number[champion] += 1
-        else:
-            champion_to_win_number[champion] = 1
+for line in lines[1:]:
+    data = line.split(",")
+    champion = data[2]
+    country = data[1]
+    champion_countries.add(country)
+    if champion in champion_to_win_number:
+        champion_to_win_number[champion] += 1
+    else:
+        champion_to_win_number[champion] = 1
 
-    print("Wimbledon Champions: ")
-    for champion, win_number in champion_to_win_number.items():
-        print(f"{champion}: {win_number}")
+
+print("Wimbledon Champions: ")
+for champion, win_number in champion_to_win_number.items():
+    print(f"{champion}: {win_number}")
 
 # TODO: Get the champions list, grab their countries, print countries in alphabetical order with no repeats
 # Format should be in capitals and their 3 letter abbreviation
+
+
+print(champion_countries)
