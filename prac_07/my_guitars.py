cc54ae71d54b7abd48e546c5ac544d7cff7e9dbf
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-07-19 11:26:22 +1000

    Update file to include functions

--- prac_07/my_guitars.py
+++ prac_07/my_guitars.py
@@ -5,22 +5,39 @@ Sam Butters
 
 from guitar import Guitar
 
-my_guitars = []
+FILENAME = "guitars.csv"
 
 
-with open("guitars.csv", "r") as infile:
-    for line in infile:
-        parts = line.strip().split(",")
-        name = parts[0]
-        year = parts[1]
-        cost = float(parts[2])
-        guitar = Guitar(name, year, cost)
-        my_guitars.append(guitar)
-        # print(my_guitars)
+def main():
+    """Read a csv file of guitars, convert them to a guitar object and display as a list."""
+    my_guitars = []
+    read_file(my_guitars)
+    print_guitars(my_guitars)
 
-for guitar in my_guitars:
-    # print(type(Guitar))
-    # print(type(my_guitars))
-    print(guitar)
+
+def read_file(my_guitars):
+    """Read a file and append a guitar object to a list."""
+    with open(FILENAME, "r") as infile:
+        for line in infile:
+            guitar = process_data(line)
+            if guitar:  # Check if process_data() returned a valid Guitar object
+                my_guitars.append(guitar)
+
+
+def process_data(line):
+    """Process a line of guitar data and return a guitar object."""
+    parts = line.strip().split(",")
+    name = parts[0]
+    year = parts[1]
+    cost = float(parts[2])
+    guitar = Guitar(name, year, cost)
+    return guitar
+
+
+def print_guitars(my_guitars):
+    """Print a list of guitars."""
+    for guitar in my_guitars:
+        print(guitar)
+
 
 main()
