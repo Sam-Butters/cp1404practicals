8364ca209020d30c0431b16d18e248c35f605c6b
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-07-19 18:15:10 +1000

    Add TODO comments for error checking

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,7 +1,7 @@
 """
 CP1404 Prac07 - Project Management
 Sam Butters
-Start time: 15:17
+Start time: 15:17 break: 18:14
 Estimated completion time: 90 minutes
 Actual completion time:
 
@@ -101,6 +101,8 @@ MENU = """- (L)oad projects
 
 def main():
     """Run the project management program."""
+
+    # TODO: add error checking
     print("Welcome to Pythonic Project Management")
     projects = []
     load_projects(projects)
@@ -130,7 +132,6 @@ def main():
         print(MENU)
         menu_choice = input(">>> ").upper()
     print(f"Would you like to save to {DEFAULT_FILE}?")
-    # TODO: add save choice
     save_choice = input(">>> (Y/n) ").upper()
     if save_choice == "Y" or save_choice == "":
         save_projects(projects)
@@ -139,7 +140,7 @@ def main():
 
 def filter_project_dates(projects):
     """Filter projects by user chosen date."""
-    filter_date_string = input("Show projects that start after date (dd/mm/yyyy): ")
+    filter_date_string = input("Show projects that start after date (dd/mm/yyyy): ")  # TODO: get valid date
     filter_date = datetime.datetime.strptime(filter_date_string, "%d/%m/%Y").date()
     for project in projects:
         start_date = datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date()
@@ -162,33 +163,30 @@ def update_project(projects):
     """Update a selected project's completion percentage and priority."""
     for number, project in enumerate(projects, start=1):
         print(f"{number} {project}")
-    project_choice = projects[int(input("Project choice: ")) - 1]
+    project_choice = projects[int(input("Project choice: ")) - 1]  # TODO: error check for valid number choice
     print(project_choice)
-    new_percentage = int(input("New percentage: "))
-    new_priority = int(input("New priority: "))
+    new_percentage = int(input("New percentage: "))  # TODO: get valid number (0-100)
+    new_priority = int(input("New priority: "))  # TODO: get valid number (1-9)
     project_choice.update_project(new_percentage, new_priority)
 
 
 def add_project(projects):
     """Add a project to projects list."""
-    #  TODO: can we add today's date as the default value? eg if start_date = "", date == today's' date
     print("Let's add a new project")
-    name = input("Name: ")
-    start_date = input("Start date (dd/mm/yyyy): ")
+    name = input("Name: ").title()  # TODO: error check name is not empty
+    start_date = input("Start date (dd/mm/yyyy): ")  # TODO: error check ddmmyyyy format is correct, can also be blank
     # Use today's date if input is empty
     if start_date == "":
         start_date = str(datetime.date.today().strftime("%d/%m/%Y"))
-    priority = int(input("Priority: "))
-    cost_estimate = float(input("Cost estimate: "))
-    percent_complete = int(input("Percentage complete: "))
+    priority = int(input("Priority: "))  # TODO: error check for valid number, 1-9
+    cost_estimate = float(input("Cost estimate: "))  # TODO: error check for valid number, cannot be <0
+    percent_complete = int(input("Percentage complete: "))  # TODO: error check for valid number, 0-100
     project = Project(name, start_date, priority, cost_estimate, percent_complete)
     projects.append(project)
 
 
 def display_projects(projects):
     """Display projects from projects list."""
-    # for project in projects:
-    #     print(project)
     print("Incomplete projects:")
     for project in projects:
         if project.completion_percentage != 100:
@@ -201,13 +199,13 @@ def display_projects(projects):
 
 def load_projects(projects):
     """Load a project from file."""
-    if not projects:  # Check if projects list is empty to determine if this is the first time running
+    # TODO: add error checking to ensure default file exists
+    if not projects:  # Check if projects list is empty to determine if this is the first time running the function
         file = DEFAULT_FILE
-    else:
+    else:  # TODO: check file name exists
         file = input("File name: ")  # Ask the user for a file name, set as default if user hits enter
         if file == "":
             file = DEFAULT_FILE
-        # TODO: add error checking to ensure file exists
     with open(file, "r") as infile:
         infile.readline()  # Bypass the headings line
         for line in infile:
