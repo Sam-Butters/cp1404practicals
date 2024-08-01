0ae5d9aae0d69aa1953f00dd8b53fdbe43d9517f
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-08-01 13:56:21 +1000

    Update programs with walkthrough changes

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,6 +1,10 @@
+"""
+Testing code for Taxi class
+"""
+
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 
 my_taxi.drive(40)
 print(my_taxi, my_taxi.get_fare())
