b4e4587a683dffd1c6d6ffc6cadacd6c1b37c672
Sam-Butters <sam.butters@my.jcu.edu.au>
2024-08-04 11:55:57 +1000

    Update Taxi with fare rounding, update Silver Service Taxi test to match

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -7,16 +7,16 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 def main():
     """Run tests for the SilverServiceTaxi class"""
-    super_fancy_taxi = SilverServiceTaxi("Super Fancy Taxi", 80, 10)
+    super_fancy_taxi = SilverServiceTaxi("Super Fancy Taxi", 80, 9)
     print(super_fancy_taxi)
-    super_fancy_taxi.drive(10)
-    print(f"Fare: ${super_fancy_taxi.get_fare():.2f}, should be $127.50")
+    super_fancy_taxi.drive(11)
+    print(f"Fare: ${super_fancy_taxi.get_fare():.2f}, should be $126.30")
     print(super_fancy_taxi)
 
     slightly_fancy_taxi = SilverServiceTaxi("Slightly Fancy Taxi", 80, 2)
     print(slightly_fancy_taxi)
-    slightly_fancy_taxi.drive(10)
-    print(f"Fare: ${slightly_fancy_taxi.get_fare():.2f}, Should be $29.10")
+    slightly_fancy_taxi.drive(11)
+    print(f"Fare: ${slightly_fancy_taxi.get_fare():.2f}, Should be $31.60")
     print(slightly_fancy_taxi)
 
 
